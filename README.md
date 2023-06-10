929a991254beac683be4361054890bd9e99dd742
Makgsoewar <makgsoewar5@gmail.com>
2023-06-10 22:46:27 +0630

    very first version of excel combine application

---
+++ README.md
@@ -0,0 +1,13 @@
+## Excel Combine
+Python spreadsheets combine application
+## Description
+
+This is a python app to combine multiple spreadsheets into one spreadsheet.
+
+### How to run
+
+* Make sure you have the pandas library installed before running this code. You can install it using `pip install pandas`.
+
+```shell
+python app.py
+```
---
+++ app.py
@@ -0,0 +1,30 @@
+import os
+import pandas as pd
+
+# Specify the directory path
+directory = "input"
+
+# Get the list of files in the directory
+file_list = os.listdir(directory)
+
+# Create an empty list to store the DataFrames
+dfs = []
+
+# Iterate over the file list
+for file_name in file_list:
+    # Check if the file is in Excel format
+    if file_name.endswith(".xlsx"):
+        # Construct the full file path
+        file_path = os.path.join(directory, file_name)
+
+        # Read the Excel file into a DataFrame
+        data = pd.read_excel(file_path)
+
+        # Append the DataFrame to the list
+        dfs.append(data)
+
+# Concatenate the DataFrames into a single DataFrame
+combined_data = pd.concat(dfs, ignore_index=True)
+
+# Write the combined_data DataFrame to a new Excel file
+combined_data.to_excel("output/combined_data.xlsx", index=False)
